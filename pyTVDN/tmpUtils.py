774fc890fdd50fc10d4937bdaa26cc7bd589c1be
jinhuaqing <jinhuaqing@hku.hk>
2021-09-25 20:29:35 +0800

    add test results for MEG

--- pyTVDN/tmpUtils.py
+++ pyTVDN/tmpUtils.py
@@ -3,11 +3,40 @@ import rpy2.robjects as robj
 from easydict import EasyDict as edict
 from numpy.linalg import inv, svd
 from scipy.signal import detrend
+from .TVDNutils import GetAmat, GetNewEst
 
 timeLims = edict()
 timeLims.st02 = [35, 95]
 timeLims.st03 = [20, 80]
 
+def GetAmatNoKernel(dXmat, Xmat):
+    """
+    simply linear regression to estimate Amat under H0: no change
+    Input: 
+        dXmat: The first derivative of Xmat, d x n matrix
+        Xmat: Xmat, d x n matrix
+    Return:
+        A d x d matrix, it is sum of n/downrate  Ai matrix
+    """
+    d, n = Xmat.shape
+    Amat = np.zeros((d, d))
+    M = Xmat.dot(Xmat.T)/n
+    XY = dXmat.dot(Xmat.T)/n
+    U, S, VT = svd(M)
+    r = np.argmax(np.cumsum(S)/np.sum(S) >= 0.999) + 1 # For real data
+    invM = U[:, :r].dot(np.diag(1/S[:r])).dot(VT[:r, :])
+    Amat = XY.dot(invM)
+    return Amat
+
+def trainIdxFn(ecpts, n):
+    """
+    return the indices of training dataset, idx from 0
+    """
+    ecptsfull = np.concatenate(([0], ecpts, [n]))
+    lenSegs = np.diff(ecptsfull)
+    kpLenSegs = np.array(lenSegs/2, dtype=int)
+    trainIdx = np.concatenate([ecptsfull[i] + np.array(list(range(1, kpLenSegs[i]+1))) for i in range(len(kpLenSegs))])
+    return (trainIdx - 1).astype(int)
 
 def supInfDist(set1, set2):
     if len(set2) == 0:
@@ -150,29 +179,99 @@ def PredDMD(Ymat, ecpts, rank=0, SegPredDMD=SegPredDMDHalf):
 
 
 # Reconstruct Xmat from results segment-wisely when using half data
-def ReconXmatSWHalf(ecpts, ndXmat, nXmat, kpidxs, eigVecs, Ymat, tStep, r, is_full=False):
+# I use Amat directly without estimating U. 
+def ReconXmatSWHalf2(ecpts, dXmat, Xmat, Ymat, time, is_full=False):
     """
     Input: 
         ecpts: Estimated change points, 
-        ndXmat: a rAct x n matrix
-        nXmat: a rAct x n matrix
-        kpidxs: The intermedian output when calculating ndXmat, nXmat
-        eigVecs: The matrix of eigen vectors of A matrix, d x d
+        dXmat: a d x n matrix
+        Xmat: a d x n matrix
         Ymat: The matrix to construct, d x n 
-        tStep: The time step
+        time: The time seq
         r: The rank setted beforehand, in most cases, r=rAct. If we have non-complex singular values, r < rAct
         if_full: Where outputing full info or not
 
     Return:
         Estimated Xmat, d x n
     """
-    #print(f"The class calls the new reconstruction function, ReconXmatNew")
-    rAct, n = ndXmat.shape
-    d, _ = Ymat.shape
+    
+    d, n = Ymat.shape
+    tStep = np.diff(time)[0]
+    trainIdxs = trainIdxFn(ecpts, n)
+    
     ecptsfull = np.concatenate(([0], ecpts, [n])) - 1
     ecptsfull = ecptsfull.astype(np.int)
     numchgfull = len(ecptsfull)
 
+    Amats = []
+    for itr in range(numchgfull-1):
+        lower = ecptsfull[itr] + 1
+        upper = ecptsfull[itr+1] + 1
+        hncol = int(upper-lower/2)
+        Ycur = dXmat[:, lower:(lower+hncol)]
+        Xcur = Xmat[:, lower:(lower+hncol)]
+        timeCur = time[lower:(lower+hncol)]
+        Amat = GetAmat(Ycur, Xcur, timeCur)/hncol
+        Amats.append(Amat)
+        
+    
+    EstXmat = np.zeros((d, n), dtype=np.complex)
+    EstXmat[:, 0] = Ymat[:, 0]
+    for i in range(1, n):
+        if i in trainIdxs:
+            EstXmat[:, i] = Ymat[:, i]
+        else:
+            matIdx = np.sum(i>=numchgfull) - 1
+            Amat = Amats[matIdx]
+            EstXmat[:, i] = Amat.dot(EstXmat[:, i-1]) * tStep + EstXmat[:,i-1]
+        
+    if is_full:
+        ReDict = edict()
+        ReDict.EstXmatReal = detrend(EstXmat.real)
+        ReDict.EstXmatRealOrg = EstXmat.real
+        ReDict.EstXmatImag = EstXmat.imag
+        ReDict.Amats = Amats
+        return ReDict
+    else:
+        return detrend(EstXmat.real)
+
+# Reconstruct Xmat from results segment-wisely when using half data
+# Only use training part to estimate U and reconsutrct the results 
+def ReconXmatSWHalf(ecpts, dXmat, Xmat, Ymat, time, paras, is_full=False):
+    """
+    Input: 
+        ecpts: Estimated change points, 
+        dXmat: a d x n matrix
+        Xmat: a d x n matrix
+        Ymat: The matrix to construct, d x n 
+        time: The time seq
+        r: The rank setted beforehand, in most cases, r=rAct. If we have non-complex singular values, r < rAct
+        if_full: Where outputing full info or not
+
+    Return:
+        Estimated Xmat, d x n
+    """
+    
+    d, n = Ymat.shape
+    r = paras.r
+    tStep = np.diff(time)[0]
+    trainIdxs = trainIdxFn(ecpts, n)
+    
+    XmatPart = Xmat[:, trainIdxs]
+    dXmatPart = dXmat[:, trainIdxs]
+    timePart = time[trainIdxs]
+    
+    Amat = GetAmat(dXmatPart, XmatPart, timePart, paras.downRate, fct=paras.fct)
+    eigVals, eigVecs = np.linalg.eig(Amat)
+    midRes = GetNewEst(dXmat, Xmat, Amat, r=paras.r, is_full=True)
+    ndXmat, nXmat = midRes.ndXmat, midRes.nXmat
+    kpidxs = midRes.kpidxs
+    rAct, _ = nXmat.shape
+    
+    ecptsfull = np.concatenate(([0], ecpts, [n])) - 1
+    ecptsfull = ecptsfull.astype(np.int)
+    numchgfull = len(ecptsfull)
+    
     ResegS = np.zeros((numchgfull-1, r), dtype=np.complex)
     for  itr in range(numchgfull-1):
         lower = ecptsfull[itr] + 1
@@ -202,31 +301,109 @@ def ReconXmatSWHalf(ecpts, ndXmat, nXmat, kpidxs, eigVecs, Ymat, tStep, r, is_fu
         LamMs[:, lower:upper] = ResegS[itr-1, ].reshape(-1, 1)
     
     ecptsf = np.concatenate([[0], ecpts, [n]])
-    fixIdxs = []
-    for i, j in zip(ecptsf[:-1], ecptsf[1:]):
-        num = int((j-i)/2)
-        fixIdxs += list(range(int(i), int(i)+num))
     EstXmat = np.zeros((d, n), dtype=np.complex)
     EstXmat[:, 0] = Ymat[:, 0]
     invEigVecsr = inv(eigVecs)[:r, :]
     eigVecsr = eigVecs[:, :r]
     for i in range(1, n):
-        if i in fixIdxs:
+        if i in trainIdxs:
             EstXmat[:, i] = Ymat[:, i]
         else:
             mTerm = np.diag(LamMs[:, i])
             rTerm = invEigVecsr.dot(EstXmat[:, i-1])
             EstXmat[:, i] = eigVecsr.dot(mTerm).dot(rTerm) * tStep + EstXmat[:,i-1]
         
+
+        
     if is_full:
         ReDict = edict()
         ReDict.EstXmatReal = detrend(EstXmat.real)
         ReDict.EstXmatRealOrg = EstXmat.real
         ReDict.EstXmatImag = EstXmat.imag
-        ReDict.LamMs = LamMs
+        ReDict.Amat = Amat
         return ReDict
     else:
-        return detrend(EstXmat.real)
+        return detrend(EstXmat.real) 
+    
+# Reconstruct Xmat from results segment-wisely when using half data
+# Use all the data to estimate U to reconstruct the results
+# def ReconXmatSWHalf(ecpts, ndXmat, nXmat, kpidxs, eigVecs, Ymat, tStep, r, is_full=False):
+#     """
+#     Input: 
+#         ecpts: Estimated change points, 
+#         ndXmat: a rAct x n matrix
+#         nXmat: a rAct x n matrix
+#         kpidxs: The intermedian output when calculating ndXmat, nXmat
+#         eigVecs: The matrix of eigen vectors of A matrix, d x d
+#         Ymat: The matrix to construct, d x n 
+#         tStep: The time step
+#         r: The rank setted beforehand, in most cases, r=rAct. If we have non-complex singular values, r < rAct
+#         if_full: Where outputing full info or not
+# 
+#     Return:
+#         Estimated Xmat, d x n
+#     """
+#     #print(f"The class calls the new reconstruction function, ReconXmatNew")
+#     rAct, n = ndXmat.shape
+#     d, _ = Ymat.shape
+#     ecptsfull = np.concatenate(([0], ecpts, [n])) - 1
+#     ecptsfull = ecptsfull.astype(np.int)
+#     numchgfull = len(ecptsfull)
+# 
+#     ResegS = np.zeros((numchgfull-1, r), dtype=np.complex)
+#     for  itr in range(numchgfull-1):
+#         lower = ecptsfull[itr] + 1
+#         upper = ecptsfull[itr+1] + 1
+#         Ycur = ndXmat[:, lower:upper]
+#         Xcur = nXmat[:, lower:upper]
+#         _, ncol = Xcur.shape
+#         hncol = int(ncol/2)
+#         lams = np.zeros(r, dtype=np.complex) + np.inf
+#         for j in range(int(rAct/2)):
+#             tY = Ycur[(2*j):(2*j+2), :hncol]
+#             tX = Xcur[(2*j):(2*j+2), :hncol]
+#             corY = tY.dot(tX.T)
+#             corX = np.trace(tX.dot(tX.T))
+#             a = np.trace(corY)/corX
+#             b = (corY[1, 0] - corY[0, 1])/corX
+#             lams[kpidxs[j]] = a + b*1j
+#         tmpIdx = np.where(lams==np.inf)[0]
+#         lams[tmpIdx] = np.conjugate(lams[tmpIdx-1])
+#         ResegS[itr, :] = lams
+#     
+#     LamMs = np.zeros((r, n), dtype=np.complex)
+#     LamMs[:, 0] = ResegS[0, :]
+#     for itr in range(1, numchgfull):
+#         lower = ecptsfull[itr-1] + 1
+#         upper = ecptsfull[itr] + 1
+#         LamMs[:, lower:upper] = ResegS[itr-1, ].reshape(-1, 1)
+#     
+#     ecptsf = np.concatenate([[0], ecpts, [n]])
+#     fixIdxs = []
+#     for i, j in zip(ecptsf[:-1], ecptsf[1:]):
+#         num = int((j-i)/2)
+#         fixIdxs += list(range(int(i), int(i)+num))
+#     EstXmat = np.zeros((d, n), dtype=np.complex)
+#     EstXmat[:, 0] = Ymat[:, 0]
+#     invEigVecsr = inv(eigVecs)[:r, :]
+#     eigVecsr = eigVecs[:, :r]
+#     for i in range(1, n):
+#         if i in fixIdxs:
+#             EstXmat[:, i] = Ymat[:, i]
+#         else:
+#             mTerm = np.diag(LamMs[:, i])
+#             rTerm = invEigVecsr.dot(EstXmat[:, i-1])
+#             EstXmat[:, i] = eigVecsr.dot(mTerm).dot(rTerm) * tStep + EstXmat[:,i-1]
+#         
+#     if is_full:
+#         ReDict = edict()
+#         ReDict.EstXmatReal = detrend(EstXmat.real)
+#         ReDict.EstXmatRealOrg = EstXmat.real
+#         ReDict.EstXmatImag = EstXmat.imag
+#         ReDict.LamMs = LamMs
+#         return ReDict
+#     else:
+#         return detrend(EstXmat.real)
     
  # Reconstruct Xmat from results segment-wisely
 def ReconXmatSW(ecpts, ndXmat, nXmat, kpidxs, eigVecs, Ymat, tStep, r, is_full=False):
